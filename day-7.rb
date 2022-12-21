c09c86a3657d11d54b10f394228e86db67a77c83
Nick Quinn <nickquinn3131@gmail.com>
2022-12-21 18:35:51 -0500

    Turns out we did not read the problem correctly and our first solution was actually correct. :)

--- day-7.rb
+++ day-7.rb
@@ -1,76 +1,41 @@
 # Author: Nick Quinn
 # Description: Solution for day 7 of 2022 Advent of Code
 
-def generate_directory_key(position)
-  if position.count == 0
-    return :/
+def walk_directory(position)
+  current_directory = $file_system[:/]
+  if position.count > 0
+    position.each do |d|
+      current_directory = current_directory[d.to_sym]
+    end
   end
-
-  key = ""
-  position.each do |p|
-    key += "/#{p}"
-  end
-  key.to_sym
+  current_directory
 end
 
-def insert_into_file_system(directory, file, value)
-  unless $file_system.has_key?(directory)
-    create_new_directory(directory)
-  end
-  dir = $file_system[directory]
-  value = value.to_i
-  dir[file] = value
+def insert_into_directory(position, key, value)
+  current_directory = walk_directory(position)
+  current_directory[key.to_sym] = value
 end
 
-def create_new_directory(key)
-  $file_system[key] = { "_total": 0 }
-end
 
-# Get value from directory
-def calculate_directory_value(directory)
-  tmp = $file_system[directory]
-  total = 0
-  tmp.each { |k,v| total += v unless k == :_total }
-  total
-end
-
-def get_directory_value(directory)
-  $file_system[directory][:_total]
-end
-
-def calculate_directory_totals
-  all_paths = Array.new
-  $file_system.each do |k,_|
-    insert_into_file_system(k, :_total, calculate_directory_value(k))
-    all_paths.append(k.to_s)
-  end
-  all_paths.reverse!
-
-  last_directory = ""
-  last_total = 0
-  all_paths.each do |d|
-    if last_directory.include?(d) and d != "/"
-    last_total += get_directory_value(last_directory.to_sym) + get_directory_value(d.to_sym)
-    insert_into_file_system(d.to_sym, :_total, last_total)
-
+def calculate_directory_totals(current_level)
+  sum = 0
+  current_level.each do |k,v|
+    if v.is_a?(Hash)
+      directory_val = calculate_directory_totals(v)
+      $total_bytes[k] = directory_val
+      sum += $total_bytes[k]
+      $test.append({k => directory_val})
     else
-      last_total = 0
+      sum += v.to_i
     end
-
-    if d.length == 2
-      tmp = get_directory_value(d.to_sym) + get_directory_value(:/)
-      insert_into_file_system(:/, :_total, tmp)
-    end
-    last_directory = d
   end
+  sum
 end
 
-def reset_position
-  Array.new
-end
-
-$file_system = { "/": {"_total": 0} }
-position = reset_position
+$file_system = { :/ => {}}
+$total_bytes = {}
+$test = []
+position = Array.new
 last_command = nil
 index = 1
 File.foreach("day-7-input.txt") do |line|
@@ -78,42 +43,40 @@ File.foreach("day-7-input.txt") do |line|
 
   # Command executed in terminal
   if line.first == "$"
-    command = line[1]
-    if command == "cd"
+    if line[1] == "cd"
       last_command = "cd"
       case line.last
       when "/"
-        position = reset_position
+        position = Array.new
       when ".."
         position.pop
       else
+        current_directory = walk_directory(position)
+        unless current_directory.has_key?(line.last)
+          insert_into_directory(position, line.last, {})
+        end
         position.append(line.last.to_sym)
       end
-    elsif command == "ls"
+    elsif line[1] == "ls"
       last_command = "ls"
-    else
-      puts "ERROR: Unknown command '#{command}'"
     end
     # Output from command in terminal
   elsif last_command == "ls"
-    if line.first != "dir"
-      insert_into_file_system(generate_directory_key(position), line.last, line.first)
+    if line.first == "dir"
+      # Save directory to filesystem. Ex: { "/": {"d": {}}}
+      # file_system[position.last][line[1]] = {}
+      insert_into_directory(position, line[1], {})
+    else
+      # Save file to filesystem. Ex: { "d": [100, "f"] }
+      insert_into_directory(position, line.last, line.first)
     end
-  else
-    puts "ERROR: Last command was not ls"
   end
 
   index += 1
 end
-calculate_directory_totals
 
-puts "File System:"
-pp $file_system
+calculate_directory_totals($file_system)
 
 answer = 0
-$file_system.each do |k,v|
-  if v[:_total] <= 100000
-    answer += v[:_total]
-  end
-end
+$test.each {|val| val.each { |k,v| answer += v unless v > 100000}}
 puts "Part 1: #{answer}"
