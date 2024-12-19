bdf1cb5391701e07119957be5ec011afffe5e171
freakynoblegas <nickquinn3131@gmail.com>
2024-12-19 16:25:58 -0500

    Solved day 4 part 2.

--- 2024/day-4.py
+++ 2024/day-4.py
@@ -6,9 +6,7 @@
 from icecream import ic
 
 
-def is_xmas(match) -> int:
-    keyword = "XMAS"
-
+def is_match(match: str, keyword: str) -> int:
     if keyword == match:
         return 1
 
@@ -19,8 +17,9 @@ def is_xmas(match) -> int:
 
     return 0
 
-def search_graph(graph: dict) -> int:
+def search_graph_for_xmas(graph: dict) -> int:
     result = 0
+    keyword = "XMAS"
     for index in graph.keys():
         for n, l in enumerate(graph[index]):
             left = n - 3 >= 0
@@ -30,31 +29,54 @@ def search_graph(graph: dict) -> int:
             # Search right
             if right:
                 match = "".join(graph[index][n:n + 4])
-                result += is_xmas(match)
+                result += is_match(match, keyword)
 
             # Search down
             if down:
                 match = [str(graph[index + offset][n]) for offset in range(1, 4)]
                 match = [graph[index][n]] + match
                 match = "".join(match)
-                result += is_xmas(match)
+                result += is_match(match, keyword)
 
             # Search diagonal
             if down and right:
                 match = [str(graph[index + offset][n + offset]) for offset in range(1, 4)]
                 match = [graph[index][n]] + match
                 match = "".join(match)
-                result += is_xmas(match)
+                result += is_match(match, keyword)
 
             if down and left:
                 match = [str(graph[index + offset][n - offset]) for offset in range(1, 4)]
                 match = [graph[index][n]] + match
                 match = "".join(match)
-                result += is_xmas(match)
+                result += is_match(match, keyword)
 
     return result
 
-def find_xmas(file_name: str) -> int:
+def search_graph_for_mas(graph: dict):
+    result = 0
+    keyword = "MAS"
+    for index in graph.keys():
+        for n, l in enumerate(graph[index]):
+            left = n - 1 >= 0
+            right = n + 1 <= (len(graph[index]) - 1)
+            down = index + 1 <= (len(graph.keys()) - 1)
+            up = index - 1 >= 0
+
+            # A will be the start point
+            if l != "A":
+                continue
+
+            if up and down and left and right:
+                first = f"{graph[index+1][n-1]}A{graph[index-1][n+1]}"
+                second = f"{graph[index+1][n+1]}A{graph[index-1][n-1]}"
+                if is_match(first, keyword) == 1 and is_match(second, keyword) == 1:
+                    result += 1
+
+    return result
+
+
+def convert_to_graph(file_name: str) -> dict:
     graph = {}
     with open(file_name) as f:
         index = 0
@@ -63,18 +85,36 @@ def find_xmas(file_name: str) -> int:
             graph[index] = line
             index += 1
 
-    return search_graph(graph)
+    return graph
 
-def run_and_assert(file_name: str, expected_result: int) -> None:
+def find_xmas(file_name: str) -> int:
+    graph = convert_to_graph(file_name)
+    return search_graph_for_xmas(graph)
+
+def find_mas(file_name: str) -> int:
+    graph = convert_to_graph(file_name)
+    return search_graph_for_mas(graph)
+
+def run_and_assert_xmas(file_name: str, expected_result: int) -> None:
     result = find_xmas(file_name)
     assert result == expected_result, f"Expected {expected_result}, got {result} for {file_name}"
     print(f"{file_name}: {result}")
 
-run_and_assert('day-4-test-input.txt', 18)
-run_and_assert('day-4-test-diag-input.txt', 2)
-run_and_assert('day-4-test-horizontal-input.txt', 3)
-run_and_assert('day-4-test-vertical-input.txt', 3)
+def run_and_assert_mas(file_name: str, expected_result: int) -> None:
+    result = find_mas(file_name)
+    assert result == expected_result, f"Expected {expected_result}, got {result} for {file_name}"
+    print(f"{file_name}: {result}")
+
+run_and_assert_xmas('day-4-test-input.txt', 18)
+run_and_assert_xmas('day-4-test-diag-input.txt', 2)
+run_and_assert_xmas('day-4-test-horizontal-input.txt', 3)
+run_and_assert_xmas('day-4-test-vertical-input.txt', 3)
 
 result = find_xmas('day-4-input.txt')
 print(f"PART 1: {result}")
 assert result == 2639
+
+run_and_assert_mas('day-4-test-x-input.txt', 2)
+
+result = find_mas('day-4-input.txt')
+print(f"PART 2: {result}")
