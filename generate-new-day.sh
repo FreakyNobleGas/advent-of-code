c9899af50a42700b9c89554f00fe6b4d421b9606
freakynoblegas <nickquinn3131@gmail.com>
2026-01-04 11:47:57 -0500

    updated python version and generate new day script

--- generate-new-day.sh
+++ generate-new-day.sh
@@ -5,13 +5,13 @@
 # Link: https://adventofcode.com/
 # Usage: bash ./generate-new-day.sh
 
-curr_year=2024
+curr_year=2025
 cd "./$curr_year" || exit 1
 
 # Go through the list of file and extract the problem from the file name so we know which
 # problem is next.
 for f in day-*.py; do
-  last_problem=${f#day-} # remove prefix
+  last_problem=${f#day-}           # remove prefix
   last_problem=${last_problem%.py} # remove suffix
 done
 
@@ -33,13 +33,14 @@ touch $solution_file_name
 touch $test_solution_file_name
 
 # Insert header
-cat << EOF >> $problem_file_name
+cat <<EOF >>$problem_file_name
 # Author: Nick Quinn
 # Description: Solution for day $next_problem of $curr_year Advent of Code
 # Problem Link: https://adventofcode.com/$curr_year/day/$next_problem
-# Interpreter: Python 3.12
+# Interpreter: Python 3.14
 EOF
 
 # Add files to Git
 cd ..
 git add "$curr_year/$problem_file_name" "$curr_year/$solution_file_name" "$curr_year/$test_solution_file_name"
+
